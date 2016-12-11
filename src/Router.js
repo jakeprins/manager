14b7cfbe3ec251d95161e42073bb2601581166c7
Jake Prins <jakeprins@outlook.com>
2016-12-11 12:35:56 +0100

    Adds shift picker to employee form

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
-import EmployeeCreate from './components/Employee';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
