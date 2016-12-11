fbb861bfcffa25024d1b6da5dad842fae8df3994
Jake Prins <jakeprins@outlook.com>
2016-12-11 11:27:35 +0100

    first setup employee form

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="06 25 09 29 09"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Submit
+          </Button>
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+      </Card>
       );
   }
 }
